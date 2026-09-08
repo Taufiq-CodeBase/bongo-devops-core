f241be8389e69edd473c02f6cbee4de8b1b5688f
Taufiq <277802546+Taufiq-CodeBase@users.noreply.github.com>
2026-09-08 21:30:01 +0600

    v1

---
+++ feature.py
@@ -0,0 +1,18 @@
+print('new feature is comming soon...')
+
+color = 'red'
+
+if(color == 'red'):
+	print('red-feature active')
+
+if(color == 'green'):
+        print('green-feature active')
+
+if(color == 'blue'):
+        print('blue-feature active')
+
+
+print('active feature above')
+
+for number in range(11):
+	print(number)
